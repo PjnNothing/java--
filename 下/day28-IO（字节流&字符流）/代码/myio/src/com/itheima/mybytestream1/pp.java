62233b84d075a1e47bae6e2087682c3ac0768533
28577 <2857750830@qq.com>
2025-02-09 22:45:58 +0800

    Java含算法

---
+++ 下/day28-IO（字节流&字符流）/代码/myio/src/com/itheima/mybytestream1/pp.java
@@ -0,0 +1,8 @@
+package com.itheima.mybytestream1;
+
+public class pp {
+    public static void main(String[] args) {
+        byte[] bytes = "黑马".getBytes();
+        System.out.println(bytes);
+    }
+}
